ff9305395eafb8340084e809d7ed7455ad4cacb6
Leo PERNELLE <lpernelle@webgeoservices.com>
2022-04-05 10:48:53 +0200

    init / Woosmap key / profile

--- App.js
+++ App.js
@@ -1,15 +1,112 @@
-import { View, Text } from "react-native";
+import React, { Component } from 'react'
+import { CreateGeofence } from './components/CreateGeofence.js'
+//import WoosmapGeofencing from 'react-native-woosmap-geofencing';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Picker
+} from 'react-native'
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Ready to test the React Native Plugin</Text>
-    </View>
-  );
+class App extends Component {
+  state = {
+    woosmapKey: "b98390e7-8750-49ac-88bd-451e7763b21c",
+    profile: ""
+  }
+
+  setWoosmapKey = () => {
+      console.log("Woosmap Key = " + this.state.woosmapKey)
+      // Set the Woosmap Project key
+  } 
+
+  initializeWoosmapGeofencing = () => {
+    console.log("Plugin is initialized {WoosmapKey: " + this.state.woosmapKey + ", profile: " + this.state.profile + "}")
+    // Initialize Woosmap Geofencing React Native Plugin
+  }
+
+  startStopTrackingProfile = (value) => {
+    this.state.profile = value;
+    switch (value) {
+      case 'liveTracking':
+        console.log('Start live tracking');
+        // start live tracking
+        break;
+      case 'passiveTracking':
+        console.log('Start passive tracking');
+        // start passive tracking
+        break;
+      case 'visitTracking':
+        console.log('Start visit tracking');
+        // start visit tracking
+        break;
+      case 'stopTracking':
+          console.log('Stop tracking');
+          // stop tracking
+          break;
+      default:
+        console.log('Stop tracking');
+        // stop tracking
+        break;
+    }
+  }
+
+ render() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <Button
+          title="Initialize Plugin"
+          color="#0000ff"
+          onPress={this.initializeWoosmapGeofencing}
+           />
+        </View>
+        <View style={styles.rowView}>
+          <TextInput 
+                style={styles.textInput} 
+                placeholder={this.state.woosmapKey} 
+                onChangeText={(value) => this.state.woosmapKey = value}/>
+          <Button style={{margin: 10}} title="SET" onPress={this.setWoosmapKey}/>
+        </View>
+        <View>
+          <Text style={styles.textInput}>
+            Change tracking profile: 
+          </Text>
+          <Picker
+          selectedValue={this.state.profile}
+          style={{ marginHorizontal: 80 }}
+          onValueChange={(value, itemIndex) => {this.startStopTrackingProfile(value)}}
+          >
+            <Picker.Item label="no tracking" value="stopTracking" />
+            <Picker.Item label="live tracking" value="liveTracking" />
+            <Picker.Item label="passive tracking" value="passiveTracking" />
+            <Picker.Item label="visit tracking" value="visitTracking" />
+          </Picker>
+        </View>  
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginHorizontal: 16,
+  },
+  rowView: {
+    flexDirection: "row",
+    margin: 10
+
+  },
+  textInput: {
+    color: "#000000",
+    fontSize: 15,
+    marginVertical: 10,
+    marginRight: 10,
+    justifyContent: 'center',
+  }
+})
+
+export default App;
---
+++ components/CreateGeofence.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { Alert, Modal, StyleSheet, Text, Pressable, View, TextInput } from "react-native";
+
+export const CreateGeofence = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <View style={styles.centeredView}>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Create a new geofence</Text>
+            <TextInput style={styles.textInput} />
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  centeredView: {
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    margin: 10
+  },
+  textInput: {
+      borderColor: "black",
+      borderWidth: 1,
+      width: 100,
+      margin: 10
+  }
+});
+
+export default CreateGeofence;
